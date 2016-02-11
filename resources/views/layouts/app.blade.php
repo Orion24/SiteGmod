effef742937cc4b22821b9b4b83c19ab97389ab3
Test <test@gmail.com>
2016-02-11 14:58:34 +0100

    Changement du nom du line de la page d'acceuil

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,7 +40,7 @@
 
                 <!-- Branding Image -->
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    Laravel
+                    Serveur Gmod Dark RP
                 </a>
             </div>
 
