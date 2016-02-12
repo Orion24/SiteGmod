3ff86737fda21a15363fbc09cb2f05aa150b6c7d
Orion24 <nicolas.bertrand74@gmail.com>
2016-02-12 20:10:31 +0100

    Vue pour l'administration
    Administration des actualités seulement

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <ul class="nav nav-tabs">
+                  <li class="active"><a data-toggle="tab" href="#actuality">Actualité</a></li>
+                  <li><a data-toggle="tab" href="#user">Utilisateur</a></li>
+                </ul>
+                <div class="tab-content">
+                  <div id="actuality" class="tab-pane fade in active">
+                    @if(Auth::check() && Auth::user()->is_admin)
+                      <div class="panel-heading">Ajouter une actualité</div>
+                      <div class="panel-body">
+                        <form action="{{ url('actuality/add') }}" method="post">
+                          <label for="disabledTextInput">Titre de l'article</label>
+                            <input type="text" class="form-control" size="35" name="articleTitle" placeholder="Titre de l'article" required><br/>
+                            <textarea class="form-control" name="content" placeholder="Contenu" required></textarea>
+                            <br />
+                            <button type="submit" class="btn btn-primary">
+                                <i class="fa fa-btn fa-user"></i>Poster
+                            </button>
+                        <form>
+                        {!! csrf_field() !!}
+                      </div>
+                    @endif
+
+                    @foreach($actualities as $actuality)
+                      <div class="panel-heading">{{$actuality->name }}</div>
+                      <div class="panel-body">
+                      {{$actuality->content}}
+                      @if(Auth::check() && Auth::user()->is_admin)
+                          <a href="{{ url('actuality/delete/'.$actuality->id.'') }}"><span class="glyphicon glyphicon-remove pull-right"></span></a>
+                          <a href="{{ url('actuality/modify/'.$actuality->id.'') }}"><span class="glyphicon glyphicon-pencil pull-right"></span></a>
+                      @endif
+                      </div>
+                    @endforeach
+                  </div>
+                  <div id="user" class="tab-pane fade">
+                    <h3>Menu 1</h3>
+                    <p>Some content in menu 1.</p>
+                  </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
